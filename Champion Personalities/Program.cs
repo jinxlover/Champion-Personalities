128429078e76782b45e5e65ce6b5b6e38dc3af91
jinxlover <dude2043099@gmail.com>
2015-01-02 18:30:51 -0600

    EndQuotes not working etc

--- Champion Personalities/Program.cs
+++ Champion Personalities/Program.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
 
-namespace Champion_Personalities
+
+
+namespace ConsoleApplication5
 {
     public class Program
     {
@@ -12,39 +16,81 @@ namespace Champion_Personalities
 
         private static void Main(string[] args)
         {
-            
-            // PUT CHAMPION QUOTES HERE
 
-            // Rammus
             var rammus = new ChampionQuote("Rammus");
             rammus.AddStartQuote("OK.");
             rammus.AddDeathQuote("Sorry, Ok");
-            rammus.AddDeathQuote();
+            rammus.AddDeathQuote("Lagging so bad!");
+            rammus.AddDeathQuote("My mom is downloading lesbian pron off utorrent, sorry team");
+            rammus.AddDeathQuote("Rito plis fix serverino");
+            rammus.AddDeathQuote("Omfg, I'm so sorry guys having a really bad game :<");
+            rammus.AddDeathQuote("Ugh I'm so bad *Sob*");
+            rammus.AddReconnectQuote("I'm back guys, internet is freaking out");
+            rammus.AddReconnectQuote("My shit if like literally on fire, sorry guys lol");
+            rammus.AddReconnectQuote("Mom turned off my router");
+            rammus.AddEndQuote("Good Game well played, I rolled all over them!");
+            rammus.AddEndQuote("GG ez boys we did it <3");
             _championQuotes.Add(rammus);
 
-            // Leblanc
-            var leblanc = new ChampionQuote("Leblanc");
-            leblanc.Add("I'm over here, Now I'm over here, Can't hit what you can't see!");
-            _championQuotes.Add(leblanc);
+            var alistar = new ChampionQuote("Alistar");
+            alistar.AddStartQuote("No! You can't milk those :D, Ok you can try <3");
+            alistar.AddStartQuote("I bet you I could milk those *Wink*");
+            alistar.AddReconnectQuote("I'm back guys, internet is freaking out");
+            alistar.AddReconnectQuote("Mom turned off my router");
+            alistar.AddReconnectQuote("My shit if like literally on fire, sorry guys lol");
+            alistar.AddDeathQuote("Ugh I'm so bad *Sob*");
+            alistar.AddDeathQuote("Omfg, I'm so sorry guys having a really bad game :<");
+            alistar.AddDeathQuote("Rito plis fix serverino");
+            alistar.AddDeathQuote("My mom is downloading lesbian pron off utorrent, sorry team");
+            alistar.AddDeathQuote("Lagging so bad!");
+            
+            _championQuotes.Add(alistar);
+
+            var annie = new ChampionQuote("Annie");
+            annie.AddStartQuote("Have you seen my bear tibbers?");
+            annie.AddReconnectQuote("I'm back guys, internet is freaking out");
+            annie.AddReconnectQuote("Mom turned off my router");
+            annie.AddReconnectQuote("My shit if like literally on fire, sorry guys lol");
+            annie.AddDeathQuote("Ugh I'm so bad *Sob*");
+            annie.AddDeathQuote("Omfg, I'm so sorry guys having a really bad game :<");
+            annie.AddDeathQuote("Rito plis fix serverino");
+            annie.AddDeathQuote("My mom is downloading lesbian pron off utorrent, sorry team");
+            annie.AddDeathQuote("Lagging so bad!");
+            
+            _championQuotes.Add(annie);
+
+            var blitzcrank = new ChampionQuote("Blitzcrank");
+            blitzcrank.AddStartQuote("Bring me dat Ashe!");
+            blitzcrank.AddReconnectQuote("I'm back guys, internet is freaking out");
+            blitzcrank.AddReconnectQuote("Mom turned off my router");
+            blitzcrank.AddDeathQuote("Ugh I'm so bad *Sob*");
+            blitzcrank.AddReconnectQuote("My shit if like literally on fire, sorry guys lol");
+            blitzcrank.AddDeathQuote("Omfg, I'm so sorry guys having a really bad game :<");
+            blitzcrank.AddDeathQuote("Rito plis fix serverino");
+            blitzcrank.AddDeathQuote("My mom is downloading lesbian pron off utorrent, sorry team");
+            blitzcrank.AddDeathQuote("Lagging so bad!");
+         
+
+
 
             // OTHER STUFF
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
-            CustomEvents.Game.OnGameEnd += Game_OnGameEnd;
+              
         }
 
         private static void Game_OnGameLoad(EventArgs args)
-		{
+        {
             Game.PrintChat("<font color=\"#1eff00\">Champion Personalities by JinxLover & WorstPing Loaded</font> - <font color=\"#00BFFF\">Loaded</font>");
             var championName = ObjectManager.Player.BaseSkinName;
-			if(Game.ClockTime < 50)
-			{
-				var cq = _championQuotes.Where(c => c.GetChampionName() == championName).First();
-				if(cq != null)
-				{
-					Utility.DelayAction.Add(5000, () => Game.Say("/all "+cq.GetRandomStartQuote()));
-				}
-			}
-		}
+            if (Game.ClockTime < 50)
+            {
+                var cq = _championQuotes.Where(c => c.GetChampionName() == championName).First();
+                if (cq != null)
+                {
+                    Utility.DelayAction.Add(5000, () => Game.Say("/all " + cq.GetRandomStartQuote()));
+                }
+            }
+        }
     }
 
     public class ChampionQuote
@@ -107,6 +153,11 @@ namespace Champion_Personalities
         public string GetRandomEndQuote()
         {
             return _endQuotes[random.Next(0, _endQuotes.Length)];
+        }
+
+        internal void AddendQuote(string p)
+        {
+            throw new NotImplementedException();
         }
     }
 }
